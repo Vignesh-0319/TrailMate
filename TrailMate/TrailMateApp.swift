32e82371eaf13e280409c1dc12a6d6fb0c50123c
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-25 09:29:20 +0530

    Initial Commit

---
+++ TrailMate/TrailMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMateApp.swift
+//  TrailMate
+//
+//  Created by SUPER CHARGE on 25/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
